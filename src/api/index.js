a11cc5fa7d6817d10d9ec32a5b2f4d0a4f1701d0
haohan-liu <2732428439@qq.com>
2026-03-07 16:30:16 +0800

    更新了上传运行

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://121.40.21.135:3000/api',
+  baseURL: 'src/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
